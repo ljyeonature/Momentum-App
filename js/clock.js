ff5a15e3a6e1da6f8c51db444d671a9d360d5393
ljyeonature <ljyeon131@gmail.com>
2023-04-17 18:13:30 +0900

    Add weather & Date

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,15 @@
+const dateDay = document.querySelector("h3#date");
 const clock = document.querySelector("h2#clock");
 
-
+function getDate() {
+    const date = new Date();
+    const days = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    const year = date.getUTCFullYear();
+    const month = date.getUTCMonth() + 1;
+    const dates = date.getUTCDate();
+    const day = days[date.getUTCDay()];
+    dateDay.innerText = `${year}.${month}.${dates}.${day}`
+}
 function getClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
@@ -11,3 +20,4 @@ function getClock() {
 // 바로 시간을 볼 수 있음 없을 때, 1초 뒤 시간이 표시
 getClock()
 setInterval(getClock, 1000);
+getDate();
